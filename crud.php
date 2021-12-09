2a68a5036c3f71a41413996e288ebfb195689145
JovanPetkovic99 <jovanpetkovicdevelopment@gmail.com>
2021-12-09 16:27:37 +0100

    Finished functionality of adding items to database

--- crud.php
+++ crud.php
@@ -3,16 +3,11 @@
 	//Glavna funkcija
 
 	function crud($db){
+		error_reporting(E_ALL);
 		formDisplay($db);
 		if(isset($_POST['fsubmit'])){
 			if(formCheck()){
-				$target_file = 'uploads/' . basename($_FILES["fimg"]["name"]);
-				error_reporting(E_ALL);
-				move_uploaded_file( $_FILES['fimg']['tmp_name'],$target_file) or die( "Could not copy file!");
-				$img = $_FILES['fimg']['tmp_name'];
-				$cena = floatval($_POST['fcena']);
-				$db->query("INSERT INTO  ");
-				echo "Uspesno unet proizvod";
+				productPush($db);
 			}
 			else{
 				echo "Proizvod se nije ubacio u sistem";
@@ -46,28 +41,46 @@
 	//Funkcija za prikazivanje forme
 
 	function formDisplay($db){
-		$kategorija = $db->query("SELECT imeKategorije FROM kategorije");
-		$tip = $db->query("SELECT ime FROM tip");	
+		$kategorija = $db->query("SELECT * FROM kategorije");
+		$tip = $db->query("SELECT * FROM tip");	
 		echo'<form method="post" id="addnew" enctype="multipart/form-data">
 			<label for="fimg">Image Source: </label>
 			<input type="file" name="fimg" required><br>
 			<label for="fcena">Cena: </label>
 			<input type="text" name="fcena" required><br>
 			<input type="submit" name="fsubmit" value="Submit">
-			<select>';
+			<select name="kategorija">';
 			// Uzimamo i ispisujemo imena kategorija proizvoda u dropdown
 			while($row = $kategorija->fetch_assoc()){
-				echo  '<option>' . $row['imeKategorije'] . '</option>';
+				echo  '<option value="' . $row['kategorija_id'] . '">'. $row['imeKategorije'] . '</option>';
 			}
 			echo '</select>';
-			echo '<select>';
+			echo '<select name="tip">';
 			//uzimamo i ispisujemo tip proizvoda u dropdown
 			while($row = $tip->fetch_assoc()){
-				echo  '<option>' . $row['ime'] . '</option>';
+				echo  '<option value="' . $row['tip_id'] . '">'. $row['ime'] . '</option>';
 			}
-			echo '</select';
+			echo '</select>';
 			echo '</form>';
 	}
 
+
+	function productPush($db){
+		$slika = 'img/' . basename($_FILES["fimg"]["name"]); //putanja gde ce se sacuvati fajl
+				$img = $_FILES['fimg']['tmp_name'];//trenutna putanja fajla
+				move_uploaded_file( $img,$slika) or die( "Could not copy file!");
+		$cena = floatval($_POST['fcena']);
+		
+		$kategorija = $_POST['kategorija'];
+		$tip = $_POST['tip'];
+		echo $cena . $slika . $kategorija . $tip;
+		$result = $db->query("INSERT INTO proizvodi (Cena,Slika,kategorija_id,tip_id) VALUES ($cena, '$slika', $kategorija, $tip)");
+		if($result){
+			echo " Success";
+		}
+		else{
+			echo $db->error;;
+		}
+	}
 
 ?>
